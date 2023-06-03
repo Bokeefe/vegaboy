a166090056d431fa032cbbcd28ced7695ed3d010
O'keefe <brendan.okeefe@charter.com>
2023-06-03 08:36:22 -0600

    add a findOne example

--- src/main/java/com/example/vegaboy/controller/BookController.java
+++ src/main/java/com/example/vegaboy/controller/BookController.java
@@ -4,8 +4,8 @@ import com.example.vegaboy.model.Book;
 import com.example.vegaboy.repository.BookRepository;
 import java.util.List;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -18,9 +18,14 @@ public class BookController {
   /*thia maps allBooks schema to the query to be used
    * findAll() is  a spring boot methid that returns every row of that type in the DB
    * */
-//  @SchemaMapping(typeName = "Query", value = "allBooks")
-    @QueryMapping
+  //  @SchemaMapping(typeName = "Query", value = "allBooks")
+  @QueryMapping
   public List<Book> allBooks() {
     return bookRepository.findAll();
+  }
+// using @QueryMapping relies on aligning the name in the schema
+  @QueryMapping
+  public Book findOne(@Argument Integer id) {
+    return bookRepository.findOne(id);
   }
 }
